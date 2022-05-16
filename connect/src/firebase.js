5261620be5e300647bbdec4e0cb8d8e6c0fcc53a
KatisCold❄️ <90704811+Kat-96@users.noreply.github.com>
2022-05-16 09:03:30 +0200

    I just added the app body

---
+++ connect/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyACFzIBz4Ci7RzkY3zxquQdyGVeIt6loXM",
+    authDomain: "connect-55d84.firebaseapp.com",
+    projectId: "connect-55d84",
+    storageBucket: "connect-55d84.appspot.com",
+    messagingSenderId: "926781749882",
+    appId: "1:926781749882:web:9dad73c93f7987530c19c3",
+    measurementId: "G-VM4RJDHH07"
+  };
