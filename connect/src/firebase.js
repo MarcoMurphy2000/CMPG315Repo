aea3ff78a62df048d201aeeefc3a085771250f7b
Francois12377 <francoiskruger55@gmail.com>
2022-05-21 23:31:10 +0200

    Ready to connect to firebase

--- connect/src/firebase.js
+++ connect/src/firebase.js
@@ -1,4 +1,5 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
 const firebaseConfig = {
     apiKey: "AIzaSyACFzIBz4Ci7RzkY3zxquQdyGVeIt6loXM",
     authDomain: "connect-55d84.firebaseapp.com",
@@ -8,3 +9,13 @@ const firebaseConfig = {
     appId: "1:926781749882:web:9dad73c93f7987530c19c3",
     measurementId: "G-VM4RJDHH07"
   };
+  const firebaseApp = firebase.initializeApp
+  (firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth(); 
+  const provide = new firebase.auth.
+  GoogleAuthProvider();
+
+  export { auth, provider };
+  export default db;
+    
