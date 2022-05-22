6741c5641708bcc9ffe14ef6bc69b9168f5f1157
EVOpro17 <jadonockhuys17@gmail.com>
2022-05-22 15:27:37 +0200

    Works with firebase

--- connect/src/firebase.js
+++ connect/src/firebase.js
@@ -1,6 +1,8 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-import firebase from "./firebase";
 
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 const firebaseConfig = {
     apiKey: "AIzaSyACFzIBz4Ci7RzkY3zxquQdyGVeIt6loXM",
     authDomain: "connect-55d84.firebaseapp.com",
@@ -10,8 +12,7 @@ const firebaseConfig = {
     appId: "1:926781749882:web:9dad73c93f7987530c19c3",
     measurementId: "G-VM4RJDHH07"
   };
-  const firebaseApp = firebase.initializeApp
-  (firebaseConfig);
+  const firebaseApp = firebase.initializeApp (firebaseConfig);
   const db = firebaseApp.firestore();
   const auth = firebase.auth(); 
   const provider = new firebase.auth.GoogleAuthProvider();
